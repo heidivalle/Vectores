fcae55d871bdfbfed59f1d996811544554221a2f
Estudiante PUCESE <heidi.valle@pucese.edu.ec>
2018-11-22 08:34:52 -0500

    Funcion  de saludo

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -11,6 +11,9 @@ namespace AppVectores
         static void Main(string[] args)
         {
             Console.WriteLine("Control de verciones kkk666 ");
+            Console.WriteLine("Control de verciones 2");
+            Console.WriteLine("Control de verciones 3");
+            Console.WriteLine("Control de verciones 4");
             Console.ReadKey();
 
         }
