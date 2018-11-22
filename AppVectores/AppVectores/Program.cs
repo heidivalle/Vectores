59513caa85425242df6fb302f497e162378c6535
Estudiante PUCESE <heidi.valle@pucese.edu.ec>
2018-11-22 09:34:35 -0500

    Funcion

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -14,8 +14,58 @@ namespace AppVectores
             Console.WriteLine("Control de verciones 2");
             Console.WriteLine("Control de verciones 3");
             Console.WriteLine("Control de verciones 4");
+            Console.WriteLine(" 3 elevado a la 4 es igual a : {0}" , potencia (3, 4) );
+            Console.ReadKey();
+
+            try
+            {
+                Console.WriteLine(" 3 elevado a la 9 es igual a : {0}", potencia(3, 9));
+            } catch (Exception ex)
+            {
+                Console.WriteLine(" Error: " + ex.Message.ToString());
+            }
+
+            try
+            {
+                Console.WriteLine(" -5 elvada a la 2 es igual  a : {0}", potencia(-5, 2));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" Error: " + ex.Message.ToString());
+            }
             Console.ReadKey();
 
         }
+        public static double potencia(double x, double y)
+        {
+            double p = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                p = p * x;
+            }
+            return p;
+        }
+
+        public static double potencia(double x, int y)
+        { 
+            if (x <= 0)
+            {
+                throw new Exception("x debe ser positivo ..");
+            }
+            if( y <= 0)
+            {
+                throw new Exception(" y debe ser positivo ....");
+            }
+            double p = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                p = p * x;
+            }
+            return p;
+            //Control de errores //
+
+        }
+
+
     }
 }
